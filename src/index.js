4d94cf4e99cf1253de369d6c2a3c4344f4686756
Lucas Cilento <lucascilento@protonmail.com>
2020-10-11 05:45:08 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,300 +2,251 @@ import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 import { render } from 'react-dom';
-import { PageHeader, Space, Button, Input, Card, List } from 'antd';
-import { PlusOutlined, RedoOutlined, UnorderedListOutlined, UserOutlined, LinkOutlined, FileOutlined, SolutionOutlined } from '@ant-design/icons';
+import { PageHeader, Space, Button, Input, Card, List, Progress, Radio, Checkbox, Modal, Slider, Breadcrumb } from 'antd';
+import { HomeOutlined, UserOutlined, FileOutlined } from '@ant-design/icons';
 
-function MainPageHeader(props) {
-    const routes = [
-        {path: 'first', breadcrumbName: 'https://trabalho-react.glitch.me'},
-    ]
+var loggedUser = 'Identificação'
 
+function MainPageHeader() {
     return <PageHeader
         style={{border: "1px solid rgb(235, 237, 240)"}}
-        onBack={() => null}
+        backIcon={<HomeOutlined/>}
+        onBack={() => {
+            loggedUser = 'Identificação'
+            renderPage()
+        }}
         title="Trabalho React"
-        breadcrumb={{routes}}
         subTitle="CRUD API Front-End"    
     />
 }
 
-function CreateStudent() {
+function CheckUser() {
     const [user, setUser] = useState({Username: ''});
-    const createStudent = () => {
-        axios.post(`https://trabalho-react.glitch.me/aluno`, {"username":user.Username}).then(res => {
-            console.log(res)
-            console.log(res.data)
+    const createUser = () => {
+        if (user.Username != '') {
+            axios.post(`https://trabalho-react.glitch.me/aluno`, {"username":user.Username}).then(res => {
+                if(res.data.erro == true) {
+                    Modal.success({content: 'Login efetuado com sucesso!'})
+                }
+                else {
+                    Modal.success({content: 'Usuário cadastrado com sucesso!'})
+                }
+                loggedUser = user.Username
+                renderPage()
+            })
+        }
+    }
+
+    return <Space direction="horizontal" style={{width: "100%", padding: "30px"}}>
+        <Space direction="vertical" style={{padding: "30px"}}>
+            <Card title="Identificação" extra={<UserOutlined/>}>
+                <div style={{padding: "30px"}}>
+                    <Space  direction="vertical">
+                        <Input addonBefore="Username:" value={user.Username} placeholder="Insira o nome de usuário"
+                            onChange={(e) => setUser({Username: e.target.value})}/>
+                        <Button type="primary" onClick={createUser}>Acessar</Button>
+                    </Space>
+                </div>
+            </Card>
+        </Space>
+    </Space>
+}
+
+function TaskManager() {
+    const [selectedTask, setSelectedTask] = useState({Id: '', Percentual: 0})
+    const [visible, setVisible] = useState({new: false, progress: false});
+    const [showID, setShowID] = useState(false)
+    const [all, setAll] = useState(true)
+    const [requestedTasks, setRequestedTasks] = useState([])
+    const [requestedPendentTasks, setRequestedPendentTasks] = useState([])
+    const [user, setUser] = useState({Username: loggedUser})
+    const [task, setTask] = useState({Title: ''})
+
+    var allLinks = []
+    var allTasks = []
+    var studentLinks = []
+    var studentTasks = []
+    var studentPendentTasks = []
+
+    const selected = (e) => {
+        setSelectedTask({Id:e.target.id.substring(0, 16), Percentual:Number(e.target.id.substring(17, e.target.id.length))})
+        if(selectedTask.Id != '') {
+            setVisible({progress:true})
+        }
+    }
+
+    const updatePercentual = (val) => {
+        setSelectedTask({Id:selectedTask.Id, Percentual:val});
+    }
+
+    const handleProgressOK = () => {
+        var url = "https://trabalho-react.glitch.me/trabalho/percentual/"+selectedTask.Id+"/"+selectedTask.Percentual
+        axios.put(url).then(res => {
             if(res.statusText == "OK") {
-                alert('Aluno cadastrado com sucesso!')
+                Modal.success({content: 'Percentual atualizado com sucesso!'})
             }
             else {
-                alert('Falha ao cadastrar aluno!')
+                Modal.success({content: 'Falha ao atualizar percentual do trabalho!'})
             }
+        }).then(res => {
+            getStudentTasks()
+            setVisible({progress:false})
         })
     }
-    return <div style={{padding: "30px"}}>
-        <Space  direction="vertical">
-            <Input addonBefore="Username:" value={user.Username} placeholder="Insira o nome de usuário"
-                onChange={(e) => setUser({Username: e.target.value})}/>
-            <Button type="primary" onClick={createStudent}>Cadastrar</Button>
-        </Space>
-    </div>
-}
 
-function GetStudents() {
-    const [students, setStudents] = useState([])
-    const getStudents = () => {
-        axios.get(`https://trabalho-react.glitch.me/aluno`).then(res => {
-            const answer = res.data;
-            console.log(res)
-            setStudents(answer)
-        })
-    }
-    const clear = () => {
-        setStudents([])
+    const handleProgressCancel = () => {
+        setVisible({progress:false})
     }
 
-    return <div style={{padding: "30px"}}>
-        <List style={{paddin: "10px"}}>
-            {students.map(student => <List.Item>{student.id}</List.Item>)}
-        </List>
-        <Space direction="horizontal">
-            <Button type="primary" onClick={getStudents}>Listar Todos</Button>
-            <Button type="primary" onClick={clear}>Limpar</Button>
-        </Space>
-    </div>
-}
+    const showNewModal = () => {
+        setVisible({new:true})
+    }
 
-function CreateTask () {
-    const [task, setTask] = useState({Title: ''});
-    const [newTaskID, setNewTaskID] = useState('')
-    const createTask = () => {
+    const handleNewOK = e => {
         axios.post(`https://trabalho-react.glitch.me/trabalho`, {"titulo":task.Title}).then(res => {
-            console.log(res)
             var ID = res.data[res.data.length - 1].id
             if(res.statusText == "OK") {
-                alert('Trabalho criado com sucesso!')
+                var url = "https://trabalho-react.glitch.me/alocacao/"+ID+"/"+loggedUser
+                axios.put(url).then(res => {
+                    if(res.statusText == "OK") {
+                        Modal.success({content: 'Trabalho criado com sucesso!\nID: '+ID})
+                    }
+                    else {
+                        Modal.success({content: 'Falha ao criar trabalho!'})
+                    }
+                }).then(res => {
+                    getStudentTasks()
+                })
             }
             else {
-                alert('Falha ao criar trabalho!')
-            }
-            setNewTaskID(ID)
-        })
-    }
-
-    return <div style={{padding: "30px"}}>
-        <Space  direction="vertical">
-            <p>ID do Trabalho: {newTaskID}</p>
-            <Input addonBefore="Título:" value={task.Title} placeholder="Insira o título"
-                onChange={(e) => setTask({Title: e.target.value})}/>
-            <Button type="primary" onClick={createTask}>Criar</Button>
-        </Space>
-    </div>
-}
-
-function GetTasks() {
-    const [allTasks, setAllTasks] = useState([])
-    const getTasks = () => {
-        axios.get(`https://trabalho-react.glitch.me/trabalho`).then(res => {
-            const answer = res.data;
-            console.log(res)
-            setAllTasks(answer)
-        })
-    }
-    const clear = () => {
-        setAllTasks([])
-    }
-
-    return <div style={{padding: "30px"}}>
-        <List style={{paddin: "10px"}}>
-            {allTasks.map(task => <List.Item>{task.titulo} - {task.percentual}%</List.Item>)}
-        </List>
-        <Space direction="horizontal">
-            <Button type="primary" onClick={getTasks}>Listar Todos</Button>
-            <Button type="primary" onClick={clear}>Limpar</Button>
-        </Space>
-    </div>
-}
-
-function UpdateTask () {
-    const [taskID, setTaskID] = useState({ID: ''});
-    const [taskProgress, setTaskProgress] = useState({Progress: ''});
-    const updateTask = () => {
-        var url = "https://trabalho-react.glitch.me/trabalho/percentual/"+taskID.ID+"/"+taskProgress.Progress
-        axios.put(url).then(res => {
-            console.log(res)
-            if(res.statusText == "OK") {
-                alert('Percentual atualizado com sucesso!')
-            }
-            else {
-                alert('Falha ao atualizar percentual do trabalho!')
+                Modal.success({content: 'Falha ao criar trabalho!'})
             }
         })
-    }
-    return <div style={{padding: "30px"}}>
-        <Space  direction="vertical">
-            <Input addonBefore="ID:" value={taskID.ID} placeholder="Insira o ID do trabalho"
-                onChange={(e) => setTaskID({ID: e.target.value})}/>
-            <Input addonBefore="Percentual:" value={taskProgress.Title} placeholder="Insira o novo percentual"
-                onChange={(e) => setTaskProgress({Progress: e.target.value})}/>
-            <Button type="primary" onClick={updateTask}>Atualizar</Button>
-        </Space>
-    </div>
-}
-
-function CreateLink () {
-    const [taskID, setTaskID] = useState({ID: ''});
-    const [user, setUser] = useState({Username: ''});
-    const createLink = () => {
-        var url = "https://trabalho-react.glitch.me/alocacao/"+taskID.ID+"/"+user.Username
-        axios.put(url).then(res => {
-            console.log(res)
-            if(res.statusText == "OK") {
-                alert('Trabalho alocado com sucesso!')
-            }
-            else {
-                alert('Falha ao alocar trabalho!')
-            }
-        })
-    }
-    return <div style={{padding: "30px"}}>
-        <Space  direction="vertical">
-            <Input addonBefore="ID:" value={taskID.ID} placeholder="Insira o ID do trabalho"
-                onChange={(e) => setTaskID({ID: e.target.value})}/>
-            <Input addonBefore="Username:" value={user.Username} placeholder="Insira usuário do aluno"
-                onChange={(e) => setUser({Username: e.target.value})}/>
-            <Button type="primary" onClick={createLink}>Alocar</Button>
-        </Space>
-    </div>
-}
-
-function GetLinks() {
-    const [allLinks, setAllLinks] = useState([])
-    const getLinks = () => {
-        axios.get(`https://trabalho-react.glitch.me/alocacao`).then(res => {
-            const answer = res.data;
-            console.log(res)
-            setAllLinks(answer)
-        })
-    }
-    const clear = () => {
-        setAllLinks([])
+        setVisible({new:false})
     }
 
+    const handleNewCancel = e => {
+        setVisible({new:false})
+    }
 
-    return <div style={{padding: "30px"}}>
-        <List style={{paddin: "10px"}}>
-            {allLinks.map(link => <List.Item>{link.idTrabalho} - {link.idAluno}</List.Item>)}
-        </List>
-        <Space direction="horizontal">
-            <Button type="primary" onClick={getLinks}>Listar Todos</Button>
-            <Button type="primary" onClick={clear}>Limpar</Button>
-        </Space>
-    </div>
-}
-
-function GetStudentTasks() {
-    const [first, setFirst] = useState(1)
-    const [destroy, setDestroy] = useState(0)
-    const [allLinks, setAllLinks] = useState([])
-    const [studentTasks, setStudentTasks] = useState([])
-    const [studentLinks, setStudentLinks] = useState([])
-    const [allTasks, setAllTasks] = useState([])
-    const [user, setUser] = useState({Username: ''});
     const getStudentTasks = () => {
-        axios.get(`https://trabalho-react.glitch.me/alocacao`).then(res => {
-            const answer = res.data;
-            console.log(res)
-            setAllLinks(answer)
+        axios.all([
+            axios.get(`https://trabalho-react.glitch.me/alocacao`),
+            axios.get(`https://trabalho-react.glitch.me/trabalho`)
+        ]).then(axios.spread((res1, res2) => {
+            const answer1 = res1.data;
+            allLinks = answer1
             allLinks.forEach(check)
-            axios.get(`https://trabalho-react.glitch.me/trabalho`).then(res => {
-                const answer = res.data;
-                console.log(res)
-                setAllTasks(answer)
-                allTasks.forEach(check)
-            })
-        })
-        if (first == 1) {
-            setFirst(0)
-        }
-        else {
-            setDestroy(1)
-        }
+            const answer2 = res2.data;
+            allTasks = answer2
+            allTasks.forEach(check)
+            setRequestedTasks(studentTasks)
+            setRequestedPendentTasks(studentPendentTasks)
+            studentLinks = []
+            studentTasks = []
+            studentPendentTasks = []
+        }))
     }
-    const clear = () => {
-        setStudentTasks([])
-        setStudentLinks([])
-        setUser('')
-        setDestroy(0)
-    }
+
     const check = (item) => {
         if (item.idAluno == user.Username) {
             studentLinks.push(item.idTrabalho)
         }
         if (studentLinks.includes(item.id)) {
             studentTasks.push(item)
-            setUser('')
+        }
+        if (studentLinks.includes(item.id) && item.percentual < 100) {
+            studentPendentTasks.push(item)
         }
     }
-    
-    return <div style={{padding: "30px"}}>
-        <Space  direction="vertical">
-            <List style={{paddin: "10px"}}>
-                {studentTasks.map(task => <List.Item>{task.titulo} - {task.percentual}%</List.Item>)}
-            </List>
-            <Input addonBefore="Username:" value={user.Username} placeholder="Insira seu nome de usuário"
-                onChange={(e) => setUser({Username: e.target.value})}/>
-            <Space direction="horizontal">
-                <Button type="primary" disabled={destroy == 1 ? true : false} onClick={getStudentTasks}>Listar Todos</Button>
-                <Button type="primary" onClick={clear}>Limpar</Button>
-            </Space>
+
+    function RequestedList() {
+
+        if(all) {
+            if(showID) {
+                return <List style={{width: 460, padding: "10px"}}>
+                    {requestedTasks.map(task => <List.Item  key={task.id} id={[task.id, task.percentual]} onClick={selected}>{task.titulo} ({task.id})<Progress percent={Number(task.percentual)}/></List.Item>)}
+                </List>
+            }
+            else {
+                return <List style={{width: 460, padding: "10px"}}>
+                    {requestedTasks.map(task => <List.Item  key={task.id} id={[task.id, task.percentual]} onClick={selected}>{task.titulo}<Progress percent={Number(task.percentual)}/></List.Item>)}
+                </List>
+            }
+        }
+        else {
+            if(showID) {
+                return <List style={{width: 460, padding: "10px"}}>
+                    {requestedPendentTasks.map(task => <List.Item  key={task.id} id={[task.id, task.percentual]} onClick={selected}>{task.titulo} ({task.id})<Progress percent={Number(task.percentual)}/></List.Item>)}
+                </List>
+            }
+            else {
+                return <List style={{width: 460, padding: "10px"}}>
+                    {requestedPendentTasks.map(task => <List.Item  key={task.id} id={[task.id, task.percentual]} onClick={selected}>{task.titulo}<Progress percent={Number(task.percentual)}/></List.Item>)}
+                </List>
+            }
+        }
+    }
+
+    return <div style={{padding: "10px"}}>
+        <Space  direction="vertical" align="start">
+            <Card title="Seus Trabalhos" extra={<FileOutlined/>}>
+                <Space  direction="vertical" align="start">
+                    <RequestedList/>
+                    <Space direction="horizontal" style={{padding: "10px"}}>
+                        <Radio.Group onChange={(e) => setAll(e.target.value)} value={all}>
+                            <Radio value={true}>Todos</Radio>
+                            <Radio value={false}>Pendentes</Radio>
+                        </Radio.Group>
+                        <Checkbox onChange={(e) => setShowID(e.target.checked)}>Mostrar IDs</Checkbox>
+                        <Button type="primary" onClick={getStudentTasks}>Listar</Button>
+                        <Button type="primary" onClick={showNewModal}>Novo</Button>
+                    </Space>
+                    <Modal title="Novo Trabalho" visible={visible.new} onOk={handleNewOK} onCancel={handleNewCancel}>
+                        <Input addonBefore="Título:" value={task.Title} placeholder="Insira o título"
+                            onChange={(e) => setTask({Title: e.target.value})}/>
+                    </Modal>
+                    <Modal title="Atualizar Progresso" visible={visible.progress} onOk={handleProgressOK} onCancel={handleProgressCancel}>
+                        <Slider min={0} max={100} onChange={updatePercentual} value={selectedTask.Percentual}/>
+                        <Progress percent={selectedTask.Percentual}/>
+                    </Modal>
+                </Space>
+            </Card>
         </Space>
     </div>
 }
 
-function MainSpace() {
+function Dashboard() {
 
-    return <Space direction="horizontal" style={{width: "100%", padding: "30px"}} align="start">
-        <Space direction="vertical" style={{padding: "30px"}}>
-            <Card title="Alunos" extra={<UserOutlined/>}>
-                <Card title="Cadastrar" extra={<PlusOutlined/>}>
-                    <CreateStudent/>
-                </Card>
-                <Card title="Listar" extra={<UnorderedListOutlined/>}>
-                    <GetStudents/>
-                </Card>
-                <Card title="Listar seus Trabalhos" extra={<SolutionOutlined/>}>
-                    <GetStudentTasks/>
-                </Card>
-            </Card>
-        </Space>
-        <Space direction="vertical" style={{padding: "30px"}}>
-            <Card title="Trabalhos" extra={<FileOutlined/>}>
-                <Card title="Criar" extra={<PlusOutlined/>}>
-                    <CreateTask/>
-                </Card>
-                <Card title="Listar" extra={<UnorderedListOutlined/>}>
-                    <GetTasks/>
-                </Card>
-                <Card title="Atualizar" extra={<RedoOutlined/>}>
-                    <UpdateTask/>
-                </Card>
-            </Card>
-        </Space>
-                <Space direction="vertical" style={{padding: "30px"}}>
-            <Card title="Alocações" extra={<LinkOutlined/>}>
-                <Card title="Alocar" extra={<PlusOutlined/>}>
-                    <CreateLink/>
-                </Card>
-                <Card title="Listar" extra={<UnorderedListOutlined/>}>
-                    <GetLinks/>
-                </Card>
+    return <Space direction="horizontal" style={{width: "100%", padding: "30px"}}>
+        <Space direction="vertical" style={{padding: "10px"}}>
+            <Card title={'Bem vindo, '+loggedUser+'!'} extra={<UserOutlined/>}>
+                <Space direction="horizontal" style={{width: "100%", padding: "10px"}}>
+                    <TaskManager/>
+                </Space>
             </Card>
         </Space>
     </Space>
 }
 
-render(<div>
-    <MainPageHeader/>
-    <MainSpace/>
-</div>, document.getElementById('root'));
+function renderPage() {
+
+    if (loggedUser == 'Identificação') {
+        render(<div>
+            <MainPageHeader/>
+            <div style={{width: '30%', margin: '0 auto'}}>
+                <CheckUser/>
+            </div>
+        </div>, document.getElementById('root'));
+    }
+    else {
+        render(<div>
+            <MainPageHeader/>
+            <div style={{width: '40%', margin: '0 auto'}}>
+                <Dashboard/>
+            </div>
+        </div>, document.getElementById('root'));
+    }
+}
+
+renderPage()
